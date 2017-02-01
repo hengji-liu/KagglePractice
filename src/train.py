729ffcd01e10db821f29714ab58bcfb2ea734f34
Hengji <hengji.liu@outlook.com>
2017-02-02 00:59:44 +1100

    breast cancer logistic vs SGD

---
+++ src/train.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import classification_report
+
+# features
+column_names = ['Sample code number',
+                'Clump Thickness',
+                'Uniformity of Cell Size',
+                'Uniformity of Cell Shape',
+                'Marginal Adhesion',
+                'Single Epithelial Cell Size',
+                'Bare Nuclei',
+                'Bland Chromatin',
+                'Normal Nucleoli',
+                'Mitoses',
+                'Class'
+                ]
+# load data
+data = pd.read_csv('../data/breast-cancer-wisconsin.data', names=column_names)
+
+# handle ?
+data = data.replace(to_replace='?', value=np.nan)
+data = data.dropna(how='any')
+
+# print(data.shape)
+#  683 * 11
+
+# split data
+X_train, X_test, y_train, y_test = train_test_split(data[column_names[1:10]], data[column_names[10]], test_size=0.25,
+                                                    random_state=33)
+# print(y_train.value_counts())
+# print(y_test.value_counts())
+
+# standardize data
+ss = StandardScaler()
+X_train = ss.fit_transform(X_train)
+X_test = ss.transform(X_test)
+
+# LR
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+lr_y_predict = lr.predict(X_test)
+
+# SGDC
+sgdc = SGDClassifier()
+sgdc.fit(X_train, y_train)
+sgdc_y_predict = sgdc.predict(X_test)
+
+# evaluate
+print('Accuracy of LR Classifier: ', lr.score(X_test, y_test))
+print(classification_report(y_test, lr_y_predict, target_names=['Benign', 'Malignant']))
+
+print('Accuracy of SGD Classifier: ', sgdc.score(X_test, y_test))
+print(classification_report(y_test, sgdc_y_predict, target_names=['Benign', 'Malignant']))
