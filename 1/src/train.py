a75a6bc3594b13ade6cce6f909e50a63b742d529
Hengji <hengji.liu@outlook.com>
2017-02-02 03:00:10 +1100

    sklearn version update

--- 1/src/train.py
+++ 1/src/train.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
